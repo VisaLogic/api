5514d02fd331368e37884bfba89501fba9b0910c
Daniel Gelling <daniel@deaannemers.nl>
2015-11-16 18:58:12 +0100

    Added ApiContract

--- src/API.php
+++ src/API.php
@@ -2,14 +2,31 @@
 
 namespace VisaLogic;
 
-class Api
+class Api implements ApiContract
 {
-
 	protected $apiKey;
 
+	/**
+	*
+	*	The constructor is responsible for setting the $this->apiKey variable
+	*
+	*	@param $apiKey
+	*	@return void
+	**/
 	public function __construct($apiKey)
 	{
 		$this->apiKey = $apiKey;
 	}
 
+	/**
+	*
+	*	The applyForVisa method is responsible for making the call to the API
+	*	to apply for the visa.
+	*
+	*	@param $apiKey
+	*	@return $application_id
+	**/
+	public function applyForVisa($personalia) {
+		
+	}
 }
---
+++ src/ApiContract.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace VisaLogic;
+
+interface ApiContract {
+
+	/**
+	*
+	*	The constructor is responsible for setting the $this->apiKey variable
+	*
+	*	@param $apiKey
+	*	@return void
+	**/
+	public function __construct($apiKey);
+
+	/**
+	*
+	*	The applyForVisa method is responsible for making the call to the API
+	*	to apply for the visa.
+	*
+	*	@param $apiKey
+	*	@return $application_id
+	**/
+	public function applyForVisa($personalia);
+
+	/**
+	*
+	*	The getStatus method is responsible for retreiving the status of an
+	*	visa that has been applied for.
+	*
+	*	@param $application_id
+	*	@return string 'WAITING' | 'REJECTED' | 'APPROVED'
+	**/
+	public function getStatus($application_id);
+
+	/**
+	*
+	*	The getVisaUrl method is responsible for retreiving the download
+	*	url of an approved visa.
+	*
+	*	@param $application_id
+	*	@return string $visa_url
+	**/
+	public function getVisaUrl($application_id);
+
+	/**
+	*
+	*	The getVisaPlain method is responsible for retreiving the plain
+	*	text visa document of an approved visa.
+	*
+	*	@param $application_id
+	*	@return string $visa
+	**/
+	public function getVisaPlain($application_id);
+
+	/**
+	*
+	*	The getRejectionReason method is responsible for retreiving the
+	*	reason for a rejected visa.
+	*
+	*	@param $application_id
+	*	@return string $visa_rejection_reason
+	**/
+	public function getRejectionReason($application_id);
+}
